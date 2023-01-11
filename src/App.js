531ceee8814ebf5c473a1643159e94d90855fe58
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-01-11 11:51:28 -0500

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <h1>CRUD App with Hooks</h1>
+        <div className="flex-row">
+          <div className="flex-large">
+            <h2>Add user</h2>
+          </div>
+          <div className="flex-large">
+            <h2>View users</h2>
+            {/* me quede en el min 7:18 */}
+          </div>
+        </div>
+
+      </div>
   );
 }
 
