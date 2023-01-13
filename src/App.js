de43c563ca366c461401fc6cc4a3107d0138b8f6
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-01-13 06:53:59 -0500

    trabajando con libreria uuid para id

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
+import UserTable from './components/UserTable';
+import { v4 as uuidv4} from 'uuid';
 
 function App() {
+
+  const usersData = [
+    {id: uuidv4(), name: 'Tania', username: 'floppydiskette'},
+    {id: uuidv4(), name: 'Craig', username: 'siliconeidolon'},
+    {id: uuidv4(), name: 'Ben', username: 'benisphere'},
+  ]
+
+  //state
+  const [users, setUsers] = useState(usersData);
+
   return (
       <div className="container">
         <h1>CRUD App with Hooks</h1>
@@ -10,7 +22,7 @@ function App() {
           </div>
           <div className="flex-large">
             <h2>View users</h2>
-            {/* me quede en el min 7:18 */}
+            <UserTable users={users}/>
           </div>
         </div>
 
